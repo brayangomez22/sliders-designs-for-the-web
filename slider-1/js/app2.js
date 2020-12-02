90d93ec111cfd6eac836a657d826f9810e19bf0a
Brayan <gomezmancobrayanalexander@gmail.com>
2020-12-02 02:24:23 -0500

    first commit

---
+++ slider-1/js/app2.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows:true,
+    dots:false,
+    infinite:true,
+    speed:500,
+    slidesToShow:1,
+    centerMode:true,
+    variableWidth:true,
+    draggable:false
+  });
+
+  $('.slider')
+   .on('beforeChange', function(event, slick, currentSlide, nextSlide){
+     $('.slick-list').addClass('do-transition')
+   })
+
+   .on('afterChange', function(){
+     $('.slick-list').removeClass('do-transition')
+   })
+});
